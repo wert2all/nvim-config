7e93848a59f0fbe7d7e762e0e4b552c0fd2db277
wert2all <wert2all@gmail.com>
2024-05-22 15:50:57 +0300

    fix: clean up which-key maps

--- lua/config/which-key/default.lua
+++ lua/config/which-key/default.lua
@@ -2,40 +2,41 @@ return {
   mode={"n", "v"},
   w = { ":w!<CR>", "Save" },
   q = { ":confirm q<CR>", "Quit" },
+
   f = {
     name = "+Search",
     f = { "<cmd>Telescope find_files<cr>", "Find File (CWD)" },
     r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
     w = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
     b = { "<cmd>Telescope buffers<cr>", "Buffers" },
-    
-    -------------
-    h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
-    H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
-    M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-    R = { "<cmd>Telescope registers<cr>", "Registers" },
-    T = { "<cmd>Telescope grep_string<cr>", "Grep String" },
     k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-    C = { "<cmd>Telescope commands<cr>", "Commands" },
-    l = { "<cmd>Telescope resume<cr>", "Resume last search" },
+    C = {
+      function ()
+        require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
+      end,
+      "Search Neovim Config",
+    },
+
+    -------------
+    -- h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+    -- H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
+    -- M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+    -- R = { "<cmd>Telescope registers<cr>", "Registers" },
+    -- T = { "<cmd>Telescope grep_string<cr>", "Grep String" },
+    -- C = { "<cmd>Telescope commands<cr>", "Commands" },
+    -- l = { "<cmd>Telescope resume<cr>", "Resume last search" },
     c = { "<cmd>Telescope git_commits<cr>", "Git commits" },
     B = { "<cmd>Telescope git_branches<cr>", "Git branches" },
     m = { "<cmd>Telescope git_status<cr>", "Git status" },
     S = { "<cmd>Telescope git_stash<cr>", "Git stash" },
-    e = { "<cmd>Telescope frecency<cr>", "Frecency" },
-        d = {
+    -- e = { "<cmd>Telescope frecency<cr>", "Frecency" },
+    d = {
       name = "+DAP",
       c = { "<cmd>Telescope dap commands<cr>", "Dap Commands" },
       b = { "<cmd>Telescope dap list_breakpoints<cr>", "Dap Breakpoints" },
       g = { "<cmd>Telescope dap configurations<cr>", "Dap Configurations" },
       v = { "<cmd>Telescope dap variables<cr>", "Dap Variables" },
       f = { "<cmd>Telescope dap frames<cr>", "Dap Frames" },
-    },
-    N = {
-      function ()
-        require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
-      end,
-      "Search Neovim Config",
     },
   },
   c = {
@@ -51,5 +52,4 @@ return {
       "Toggle Codeium",
     } 
   }
-
 }
